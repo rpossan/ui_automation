e5ea7aa3e98d6ca0291a22d555a4be506497feff
Ronaldo Possan <ronaldo.possan@gmail.com>
2017-04-05 14:36:38 +0000

    Adds instagram multiple session tests

---
+++ test/instagram_test.exs
@@ -0,0 +1,23 @@
+defmodule InstagramTest do
+	use ExUnit.Case
+	use ExUnit.Case
+	use Hound.Helpers
+
+	hound_session()
+
+	test "Should open Instagram web in multiple sessions" do
+		url = "https://www.instagram.com"
+		navigate_to(url)
+
+		change_sessiont_to :another
+
+		navigate_to(url)
+
+		assert url == current_url()
+
+		change_to_default_session()
+
+		assert url == current_url()
+	end
+
+end
