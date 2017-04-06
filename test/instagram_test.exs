326cc75b32be47745f2f6037be3c2c7ac2192b65
Ronaldo Possan <ronaldo.possan@gmail.com>
2017-04-06 13:28:28 +0000

    Code adjustment

--- test/instagram_test.exs
+++ test/instagram_test.exs
@@ -1,47 +1,45 @@
 defmodule InstagramTest do
-	use ExUnit.Case
-	use ExUnit.Case
-	use Hound.Helpers
+  use ExUnit.Case
+  use ExUnit.Case
+  use Hound.Helpers
 
-	hound_session()
+  hound_session()
 	
-	@url "https://www.instagram.com/"
+  @url "https://www.instagram.com/"
 
-	def user do
-		get_system("INSTAUSER")
-	end
+  def user do
+    get_system("INSTAUSER")
+  end
 
-	def password do
-		get_system("INSTAPASS")
-	end
+  def password do
+    get_system("INSTAPASS")
+  end
 
-	defp get_system(var) do
-		System.get_env(var)
-	end
+  defp get_system(var) do
+    System.get_env(var)
+  end
 
-	test "Validates system variables" do
-		assert user() != nil
-		assert password() != nil
-	end
+  test "Validates system variables" do
+    assert user()
+    assert password()
+  end
 
+  test "Should open Instagram web main page and user pager in multiple sessions" do
+    navigate_to @url 
+    change_session_to :user_page
+    navigate_to "http://www.instagram.com/" <> user()
+    refute current_url() == @url
+    change_to_default_session()
+    assert current_url() == @url
+  end
 
-	test "Should open Instagram web main page and user pager in multiple sessions" do
-		navigate_to(@url)
-		change_session_to :user_page
-		navigate_to("http://www.instagram.com/" <> user())
-		assert current_url() != @url
-		change_to_default_session()
-		assert current_url() == @url
-	end
-
-	test "Given an user and password I should log in on Instagram web" do
-		
-		navigate_to(@url)
-		username_input = find_element(:name, "username")
-		fill_field(username_input, user())
-		fill_field(find_element(:name, "password"), password())
-		submit_element(username_input)
-		find_element(:class, "_etslc")
-	end
+  test "Given an user and password I should log in on Instagram web" do
+    navigate_to @url
+    username_input = find_element(:name, "username")
+    fill_field(username_input, user())
+    fill_field(find_element(:name, "password"), password())
+    submit_element username_input
+    find_element(:class, "_etslc")
+  end
 
 end
